0e4eafe2cfba0b8f8b4459b708d12566b0ece696
Krishnapriya Ranga Nathan <krishnapriyaranganathan@gmail.com>
2018-04-02 22:02:36 +0800

    Initial commit

--- RailwaySystem/Form1.Designer.vb
+++ RailwaySystem/Form1.Designer.vb
@@ -22,9 +22,17 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(822, 262)
+        Me.Name = "Form1"
+        Me.Text = "Book Tickets"
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
